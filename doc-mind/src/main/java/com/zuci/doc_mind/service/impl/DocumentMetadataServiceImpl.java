d8e097e8088b7ce2efb275e1081dc1c2bb34aa3a
Abhishek <abhishek.chandra@zucisystems.com>
2026-09-21 15:03:50 +0530

    modified

--- doc-mind/src/main/java/com/zuci/doc_mind/service/impl/DocumentMetadataServiceImpl.java
+++ doc-mind/src/main/java/com/zuci/doc_mind/service/impl/DocumentMetadataServiceImpl.java
@@ -1,6 +1,7 @@
 package com.zuci.doc_mind.service.impl;
 
 import com.zuci.doc_mind.dto.DocumentResponseDto;
+import com.zuci.doc_mind.repository.DocumentMetadataRepository;
 import com.zuci.doc_mind.service.DocumentMetadatService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,6 +12,9 @@ import org.springframework.web.multipart.MultipartFile;
 @Service
 @RequiredArgsConstructor
 public class DocumentMetadataServiceImpl implements DocumentMetadatService {
+
+    private final DocumentMetadataRepository documentMetadataRepository;
+
     @Override
     public DocumentResponseDto uploadAndProcess(MultipartFile file) {
         return null;
